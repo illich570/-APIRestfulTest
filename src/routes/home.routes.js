1a98fac16e1282a0dc00ac2765f7aea5dcf7b006
Illich Rada <illich570@gmail.com>
2020-03-08 00:26:32 -0400

    [ADD] First config added

---
+++ src/routes/home.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+module.exports = function({HomeController}){
+  const router = Router();
+
+
+  router.get('/', HomeController.index);
+
+
+  return router;
+};
